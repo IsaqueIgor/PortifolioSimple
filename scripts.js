e450d25456dc27eebfc27286ecb13e9aa17f587d
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-07-18 11:16:33 -0300

    header changes and footer

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
   var scroll_position = window.scrollY;
   if (scroll_position > 250) {
-    header.style.backgroundColor = '#29323c';
+    header.style.backgroundColor = '#F5EFEF';
   } else {
     header.style.backgroundColor = 'transparent';
   }
